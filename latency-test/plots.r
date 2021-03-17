db5eddb1f54711f3d1943fbcd4a6e4f0144db399
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2021-03-17 16:04:45 +0100

    Do more measurements.

--- latency-test/plots.r
+++ latency-test/plots.r
@@ -3,26 +3,38 @@ library(purrr)
 library(ggplot2)
 
 reads <- seq(1, 8)
+sizes <- c(16, 128, 257)
 
 df <- purrr::map_dfr(reads, function(n) {
-  file <- paste("read-", n, ".csv", sep="")
-  read.csv(file) %>%
-    mutate(read_no = n)
+  purrr::map_dfr(sizes, function(size) {
+    file <- paste(size, "mb/", "read-", n, ".csv", sep="")
+    read.csv(file) %>% mutate(read_no = n, size_mb = size)
+  })
 })
 
 df.hist <- df %>%
-  filter(read_no < 8) %>%
   mutate(
-    read_no = factor(read_no),
-    latency_group = round(diff_us / 10000)
-  ) %>%
-  group_by(read_no, latency_group) %>%
-  summarize(latency_count = n()) %>%
-  mutate(latency_ms = latency_group * 10)
+    read_f = factor(read_no),
+    size_f = factor(size_mb),
+    latency_group = diff_us / 1000
+  )
 
-ggplot(df.hist, aes(x = latency_ms, y = latency_count, color = read_no)) +
-  geom_point(shape = 3) +
-  geom_smooth() +
-  scale_x_continuous(breaks = seq(100, 2000, 100))
-
+ggplot(df.hist, aes(x = latency_group)) +
+  geom_histogram() +
+  facet_grid(rows = vars(read_f), cols = vars(size_f))
 ##  ggsave("latency-hist.pdf")
+
+bw <- df.hist %>% group_by(read_f, size_f) %>%
+  summarize(bw = (sum(read_byte) / 1024 / 1024) / (overall_us[1] / 1000 / 1000))
+
+min_lat <- df.hist %>%
+  group_by(size_mb) %>%
+  summarize(
+    min_lat = min(latency_group),
+    ) %>% ## model: time = 10ms seek + (size / 100mb/s) read
+  mutate(model = 10 + (size_mb / 100) * 1000)
+
+ggplot(min_lat, aes(x = model, y = min_lat, color = factor(size_mb))) +
+  geom_point() +
+  geom_abline() +
+  geom_text(aes(label = min_lat), nudge_y = 0.02 * max(min_lat$min_lat))
